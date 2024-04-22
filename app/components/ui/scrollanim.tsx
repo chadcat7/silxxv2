eb90b89ee1364f2d4d15801ef8a3d3559bea81e3
nam <namish@tuta.io>
2024-04-22 20:47:18 +0530

    tired

--- app/components/ui/scrollanim.tsx
+++ app/components/ui/scrollanim.tsx
@@ -35,7 +35,7 @@ export const ContainerScroll = ({
 
   return (
     <div
-      className="h-[60rem] md:h-[60rem] flex items-center justify-center relative p-2 md:p-20"
+      className="h-[60rem] md:h-[60rem] w-full md:w-auto flex items-center justify-center relative p-2 md:p-20"
       ref={containerRef}
     >
       <div
